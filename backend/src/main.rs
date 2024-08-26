db6ab1616a84cee1a4bca79640c2352db399c876
Steveplays28 <djspaargaren@outlook.com>
2024-08-26 22:36:16 +0200

    feat: :sparkles: Add server-side rendering

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,103 @@
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
+
 use backend::routes::index::{self};
+use common::ui::components::{switch, NavBar, Route};
 use rocket::fs::relative;
 use rocket::fs::FileServer;
+use rocket::http::Method;
+use rocket::http::Status;
+use rocket::response::content::RawHtml;
+use rocket::route::{Handler, Outcome};
+use rocket::{Data, Request};
+use yew::prelude::*;
+use yew::ServerRenderer;
+use yew_router::history::AnyHistory;
+use yew_router::history::History;
+use yew_router::history::MemoryHistory;
+use yew_router::prelude::*;
 
 #[macro_use]
 extern crate rocket;
+
+#[derive(Properties, PartialEq, Debug)]
+struct ServerAppProps {
+    path: PathBuf,
+}
+
+#[derive(Clone)]
+struct CustomHandler();
+
+#[rocket::async_trait]
+impl Handler for CustomHandler {
+    async fn handle<'r>(&self, req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r> {
+        let route = req.segments::<PathBuf>(0..).unwrap();
+        let route_result = Route::from_str(route.to_str().unwrap());
+        if route_result.is_ok() {
+            Outcome::from(
+                req,
+                render(PathBuf::from_str(route.to_str().unwrap()).unwrap()).await,
+            )
+        } else {
+            Outcome::forward(data, Status::Accepted)
+        }
+    }
+}
+
+impl From<CustomHandler> for Vec<rocket::route::Route> {
+    fn from(val: CustomHandler) -> Self {
+        vec![
+            rocket::route::Route::new(Method::Get, "/", val.clone()),
+            rocket::route::Route::new(Method::Get, "/<path..>", val),
+        ]
+    }
+}
 
 #[get("/status")]
 fn status() -> &'static str {
     "OK"
 }
 
+#[get("/<path..>")]
+async fn render(path: PathBuf) -> RawHtml<String> {
+    let index_html = fs::read_to_string("../frontend/dist/index.html").unwrap();
+    let server_app_props = ServerAppProps { path };
+    let content_html = ServerRenderer::<ServerApp>::with_props(|| server_app_props)
+        .render()
+        .await;
+    RawHtml(index_html.replace("<body>", &format!("<body>{}", content_html)))
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![status])
-        .mount("/index/", routes![index::projects])
+        .mount("/index", routes![index::projects])
         .mount(
             "/projects",
             FileServer::from(relative!("resources/projects/")),
         )
+        .mount(
+            "/",
+            FileServer::from(relative!("../frontend/dist/")).rank(0),
+        )
+        .mount("/", CustomHandler())
+}
+
+#[function_component(ServerApp)]
+fn server_app(props: &ServerAppProps) -> Html {
+    let history = AnyHistory::from(MemoryHistory::new());
+    history.push(format!("/{}", props.path.to_str().unwrap()));
+
+    html! {
+        <>
+            <Router history={history}>
+                <NavBar />
+
+                // Must be child of <Router>
+                <Switch<Route> render={switch} />
+            </Router>
+        </>
+    }
 }
---
+++ common/src/ui/components.rs
@@ -0,0 +1,212 @@
+use gloo_net::http::Request;
+use strum_macros::EnumString;
+use yew::{platform::spawn_local, prelude::*};
+use yew_router::prelude::*;
+
+use crate::data::project::Project;
+
+#[derive(Debug, Clone, Copy, PartialEq, Routable, EnumString)]
+pub enum Route {
+    #[strum(serialize = "", ascii_case_insensitive)]
+    #[at("/")]
+    Home,
+    #[strum(ascii_case_insensitive)]
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+    #[strum(ascii_case_insensitive)]
+    #[at("/projects")]
+    Projects,
+    #[strum(ascii_case_insensitive)]
+    #[at("/contact")]
+    Contact,
+}
+
+pub fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <Home />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+        Route::Projects => html! {
+            <Projects />
+        },
+        Route::Contact => html! {
+            <Contact />
+        },
+    }
+}
+
+#[function_component(NavBar)]
+pub fn nav_bar() -> Html {
+    html! {
+        <>
+            <div class="nav-bar">
+                <div class="nav-bar-element">
+                    <Link<Route> to={Route::Home}>
+                        <p>{ "Home" }</p>
+                    </Link<Route>>
+                </div>
+                <div class="nav-bar-element">
+                    <Link<Route> to={Route::Contact}>
+                        <p>{ "Contact" }</p>
+                    </Link<Route>>
+                </div>
+
+                <div class="nav-bar-element socials">
+                    <a href="https://github.com/Steveplays28/portfolio-website-rs" target="_blank" rel="noopener noreferrer">
+                        <img src="/resources/logos/github-mark-white.svg" />
+                    </a>
+                    <a href="https://discord.gg/KbWxgGg" target="_blank" rel="noopener noreferrer">
+                        <img src="/resources/logos/discord-mark-white.svg" />
+                    </a>
+                    <a href="https://www.youtube.com/steveplays28" target="_blank" rel="noopener noreferrer">
+                        <img src="/resources/logos/yt_icon_mono_dark.png" />
+                    </a>
+                    <a href="https://www.patreon.com/steveplays28" target="_blank" rel="noopener noreferrer">
+                        <img src="/resources/logos/Digital-Patreon-Logo_White.png" />
+                    </a>
+                    <a href="https://ko-fi.com/steveplays" target="_blank" rel="noopener noreferrer">
+                        <img src="/resources/logos/kofi_s_logo_nolabel.svg" />
+                    </a>
+                </div>
+            </div>
+        </>
+    }
+}
+
+#[function_component(Home)]
+fn home() -> Html {
+    let projects_option = use_state(|| None);
+    {
+        let projects = projects_option.clone();
+        use_effect(move || {
+            if projects.is_none() {
+                spawn_local(async move {
+                    let response = Request::get("/index/projects").send().await.unwrap();
+                    if response.ok() {
+                        projects.set(Some(Ok(response.json::<Vec<Project>>().await.unwrap())));
+                    } else {
+                        projects.set(Some(Err(format!(
+                            "Error fetching data {} ({})",
+                            response.status(),
+                            response.status_text()
+                        ))));
+                    }
+                });
+            }
+
+            || {}
+        });
+    }
+    let initial_project_animation_delay_seconds: f32 = 0.25;
+    let mut index: i32 = 0;
+    html! {
+        <>
+            <div class="bio-container">
+                <a href="https://github.com/Steveplays28" target="_blank" rel="noopener noreferrer" class="profile-picture-container">
+                    <img src="/resources/logos/steve_profile_picture.png" alt="Profile picture" class="profile-picture" />
+                    <img src="/resources/logos/github-mark-white-background.svg" alt="GitHub out link" class="github" />
+                </a>
+
+                <p class="bio">
+                    { "Hi, I'm Steve!" } <br/>
+                    { "I like making games in Godot, as well as making Minecraft mods." } <br/><br/>
+                    { "I'm currently maintaining 6 Minecraft mods, a Factorio mod, and a Satisfactory mod. I'm also contributing to other Minecraft projects, such as " } <a href="https://modrinth.com/mod/forgero" target="_blank" rel="noopener noreferrer"> { "Forgero" } </a> { " and "} <a href="https://modrinth.com/mod/distanthorizons" target="_blank" rel="noopener noreferrer"> { "Distant Horizons" } </a> { "." }
+                </p>
+            </div>
+
+            <div class="projects">
+            {
+                if let Some(Ok(projects)) = projects_option.as_ref() {
+                    projects.iter().map(|project| {
+                        let style = format!("animation-delay: {seconds}s; background-image: url({image});", seconds = (index as f32) / 4.0 + initial_project_animation_delay_seconds, image = project.image.clone());
+                        index += 1;
+
+                        html! {
+                            <a href={project.link.clone()} target="_blank" rel="noopener noreferrer" key={project.name.clone()} class="project" style={style}>
+                                <p class="project-title">{ project.name.clone() }</p>
+                            </a>
+                        }
+                    }).collect::<Html>()
+                } else {
+                    html ! {
+                        <p class="no-response">{ "No response from the server, you may be offline." }</p>
+                    }
+                }
+            }
+            </div>
+        </>
+    }
+}
+
+#[function_component(Projects)]
+pub fn projects() -> Html {
+    html! {
+        <>
+            <h1 class="title">{"Projects"}</h1>
+        </>
+    }
+}
+
+#[function_component(Contact)]
+pub fn contact() -> Html {
+    html! {
+        <>
+            <div class="contact-text">
+                <p class="title">{ "Reach out to me" }</p> <br/>
+                <p class="subtitle">{ "I respond most quickly on Discord, GitHub and Patreon, but I try my best to respond on all platforms." }</p>
+            </div>
+
+            <div class="socials contact-socials">
+                <a href="https://www.patreon.com/steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/Digital-Patreon-Logo_White.png" />
+                </a>
+                <a href="https://ko-fi.com/steveplays" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/kofi_s_logo_nolabel.svg" />
+                </a>
+                <div class="gap"></div>
+
+                <a href="https://github.com/Steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/github-mark-white.svg" />
+                </a>
+                <a href="https://gitlab.com/steveplays" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/gitlab-logo-700.svg" />
+                </a>
+                <div class="gap"></div>
+
+                <a href="https://steveplays.itch.io" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/itchio-logo-textless-white.svg" />
+                </a>
+                <a href="https://modrinth.com/user/Steveplays" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/modrinth-logo-white.svg" />
+                </a>
+                <a href="https://www.curseforge.com/members/steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/curseforge-logo-white.svg" />
+                </a>
+                <div class="gap"></div>
+
+                <a href="https://www.youtube.com/steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/yt_icon_mono_dark.png" />
+                </a>
+                <a href="https://twitch.tv/steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/TwitchGlitchWhite.svg" />
+                </a>
+                <div class="gap"></div>
+
+                <a href="https://discord.gg/KbWxgGg" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/discord-mark-white.svg" />
+                </a>
+                <a href="https://rvlt.gg/gYXfebk5" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/revolt_chat_logo_white.png" />
+                </a>
+                <a href="https://mastodon.gamedev.place/@steveplays" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/mastodon-logo-white.svg" />
+                </a>
+                <a href="https://twitter.com/steveplays28" target="_blank" rel="noopener noreferrer">
+                    <img src="/resources/logos/twitter-logo-white.svg" />
+                </a>
+            </div>
+        </>
+    }
+}
