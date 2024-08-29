1b5d2bf76f8d18e7f100d4c78e4afb6ac1030cba
Steveplays28 <djspaargaren@outlook.com>
2024-08-30 00:16:13 +0200

    fix: :bug: Fix default CLI arguments and VSCode task arguments

--- backend/src/cli/arguments.rs
+++ backend/src/cli/arguments.rs
@@ -7,10 +7,10 @@ use static_init::dynamic;
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// The path of the backend's resources directory.
-    #[arg(short, long, value_hint = ValueHint::DirPath, default_value = "./resources")]
+    #[arg(short, long, value_hint = ValueHint::DirPath, default_value = "backend/resources")]
     pub backend_resources_path: PathBuf,
     /// The path of the frontend's dist directory.
-    #[arg(short, long, value_hint = ValueHint::DirPath, default_value = "../frontend/dist")]
+    #[arg(short, long, value_hint = ValueHint::DirPath, default_value = "frontend/dist")]
     pub frontend_dist_path: PathBuf,
 }
 
