de097c354030238e348188b45ebc3d65a14c943c
Darion Spaargaren <djspaargaren@outlook.com>
2025-12-29 20:33:07 +0100

    fix: :rotating_light: Implement `PartialOrd` manually instead of deriving it

--- common/src/data/project.rs
+++ common/src/data/project.rs
@@ -1,27 +1,27 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, PartialEq, Eq, PartialOrd)]
+#[derive(Deserialize, Serialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
 pub struct Author {
     pub name: String,
     pub website: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, PartialEq, Eq, PartialOrd)]
+#[derive(Deserialize, Serialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
 pub struct Images {
     pub icon: Option<String>,
     pub banner: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, PartialEq, Eq, PartialOrd)]
+#[derive(Deserialize, Serialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
 pub struct Links {
     pub website: Option<String>,
     pub repository: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, PartialEq, Eq, PartialOrd)]
+#[derive(Deserialize, Serialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
 pub struct Project {
     pub name: String,
@@ -30,6 +30,12 @@ pub struct Project {
     pub images: Option<Images>,
     pub links: Option<Links>,
     pub tags: Option<Vec<String>>,
+}
+
+impl PartialOrd for Project {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Ord for Project {
